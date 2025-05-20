fab7adaca7119a95b9bed62beda5149894d7a388
marcus_kang <lebuet89@gmail.com>
2025-05-21 01:18:37 +0900

    AOP concept added

---
+++ src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -0,0 +1,29 @@
+package hello.hello_spring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    /**
+     * AOP 예시
+     * 기능 실행 소요시간을 찍는 timestamp 생성
+     */
+    @Around("execution(* hello.hello_spring..*(..))")
+    public Object excute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try{
+            return joinPoint.proceed();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+
+    }
+}
